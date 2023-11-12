9ec9455598dad16920f33d3a4e9e9c1eed7e974b
MSI <aleksander.inglot98@gmail.com>
2023-11-12 13:22:48 +0100

    feature/dropped authorization

--- src/main/java/com/github/olson1998/synthwave/service/authorizationserver/application/oauth2/config/OAuth2RepositoriesConfig.java
+++ src/main/java/com/github/olson1998/synthwave/service/authorizationserver/application/oauth2/config/OAuth2RepositoriesConfig.java
@@ -1,25 +1,18 @@
 package com.github.olson1998.synthwave.service.authorizationserver.application.oauth2.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.olson1998.synthwave.service.authorizationserver.application.datasource.repository.*;
 import com.github.olson1998.synthwave.service.authorizationserver.application.oauth2.props.RegistrationClientImplProperties;
 import com.github.olson1998.synthwave.service.authorizationserver.domain.port.oauth2.repository.*;
 import com.github.olson1998.synthwave.service.authorizationserver.domain.port.request.repository.UserPropertiesRepository;
 import com.github.olson1998.synthwave.service.authorizationserver.domain.service.oauth2.*;
 import com.github.olson1998.synthwave.service.authorizationserver.domain.service.request.service.UserPropertiesService;
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @Configuration
 public class OAuth2RepositoriesConfig {
-
-    @Bean
-    public OAuth2TokenMapper oAuth2TokenMapper(JwtDecoder jwtDecoder,
-                                               ObjectMapper objectMapper){
-        return new OAuth2TokenMapperImpl(jwtDecoder, objectMapper);
-    }
 
     @Bean
     public RegisteredClientMapper registeredClientMapper(){
@@ -27,26 +20,19 @@ public class OAuth2RepositoriesConfig {
     }
 
     @Bean
-    public OAuth2AuthorizationMapper oAuth2AuthorizationMapper(ObjectMapper objectMapper,
-                                                               OAuth2TokenMapper oAuth2TokenMapper,
-                                                               RegisteredClientMapper registeredClientMapper){
-        return new OAuth2AuthorizationMapperImpl(objectMapper, oAuth2TokenMapper, registeredClientMapper);
-    }
-
-    @Bean
-    public RegistrationClientRepository registrationClientRepository(PasswordEncoder passwordEncoder,
-                                                                     RegistrationClientImplProperties registrationClientImplProperties){
+    public RegistrationClientRepository registrationClientRepository(@NonNull PasswordEncoder passwordEncoder,
+                                                                     @NonNull RegistrationClientImplProperties registrationClientImplProperties){
         return new RegistrationClientService(passwordEncoder, registrationClientImplProperties);
     }
 
     @Bean
-    public SynthWaveRegisteredClientRepository synthWaveRegisteredClientRepository(RegisteredClientMapper registeredClientMapper,
-                                                                                   RegistrationClientRepository registrationClientRepository,
-                                                                                   UserPropertiesJpaRepositoryProxy userPropertiesSourceRepository,
-                                                                                   RedirectURIJpaRepositoryProxy redirectURIJpaRepositoryProxy,
-                                                                                   RegisteredClientJpaRepositoryProxy synthWaveRegisteredClientJpaRepositoryProxy,
-                                                                                   AuthorizationGrantTypeBindJpaRepositoryProxy authorizationGrantTypeBindJpaRepositoryProxy,
-                                                                                   ClientAuthenticationMethodJpaRepositoryProxy clientAuthenticationMethodJpaRepositoryProxy){
+    public SynthWaveRegisteredClientRepository synthWaveRegisteredClientRepository(@NonNull RegisteredClientMapper registeredClientMapper,
+                                                                                   @NonNull RegistrationClientRepository registrationClientRepository,
+                                                                                   @NonNull UserPropertiesJpaRepositoryProxy userPropertiesSourceRepository,
+                                                                                   @NonNull RedirectURIJpaRepositoryProxy redirectURIJpaRepositoryProxy,
+                                                                                   @NonNull RegisteredClientJpaRepositoryProxy synthWaveRegisteredClientJpaRepositoryProxy,
+                                                                                   @NonNull AuthorizationGrantTypeBindJpaRepositoryProxy authorizationGrantTypeBindJpaRepositoryProxy,
+                                                                                   @NonNull ClientAuthenticationMethodJpaRepositoryProxy clientAuthenticationMethodJpaRepositoryProxy){
         return new SynthWaveRegisteredClientService(
                 registeredClientMapper,
                 registrationClientRepository,
@@ -60,32 +46,20 @@ public class OAuth2RepositoriesConfig {
     }
 
     @Bean
-    public UserDetailsRepository synthWaveUserDetailsRepository(UserPropertiesJpaRepositoryProxy synthWaveUserDataSourceRepositoryProxy){
+    public UserDetailsRepository synthWaveUserDetailsRepository(@NonNull UserPropertiesJpaRepositoryProxy synthWaveUserDataSourceRepositoryProxy){
         return new UserDetailsService(synthWaveUserDataSourceRepositoryProxy);
     }
 
     @Bean
-    public OAuth2AuthorizationRepository oAuth2AuthorizationRepository(OAuth2TokenMapper oAuth2TokenMapper,
-                                                                       OAuth2AuthorizationMapper oAuth2AuthorizationMapper,
-                                                                       OAuth2TokenJpaRepositoryProxy oAuth2TokenJpaRepositoryProxy,
-                                                                       RedirectURIJpaRepositoryProxy redirectURIJpaRepositoryProxy,
-                                                                       OAuth2AuthorizationJpaRepositoryProxy oAuth2AuthorizationJpaRepositoryProxy,
-                                                                       RegisteredClientJpaRepositoryProxy registeredClientJpaRepositoryProxy){
-        return new OAuth2AuthorizationServiceInstance(
-                oAuth2TokenMapper,
-                oAuth2AuthorizationMapper,
-                oAuth2TokenJpaRepositoryProxy,
-                redirectURIJpaRepositoryProxy,
-                oAuth2AuthorizationJpaRepositoryProxy,
-                registeredClientJpaRepositoryProxy
-        );
+    public OAuth2AuthorizationRepository oAuth2AuthorizationRepository(){
+        return new SimpleOAuth2AuthorizationService();
     }
 
     @Bean
-    public UserPropertiesRepository userPropertiesRepository(PasswordEncoder passwordEncoder,
-                                                         UserPropertiesJpaRepositoryProxy userPropertiesJpaRepositoryProxy,
-                                                         UserPasswordJpaRepositoryProxy userPasswordJpaRepositoryProxy,
-                                                         AffiliationJpaRepositoryProxy affiliationJpaRepositoryProxy){
+    public UserPropertiesRepository userPropertiesRepository(@NonNull PasswordEncoder passwordEncoder,
+                                                             @NonNull UserPropertiesJpaRepositoryProxy userPropertiesJpaRepositoryProxy,
+                                                             @NonNull UserPasswordJpaRepositoryProxy userPasswordJpaRepositoryProxy,
+                                                             @NonNull AffiliationJpaRepositoryProxy affiliationJpaRepositoryProxy){
         return new UserPropertiesService(
                 passwordEncoder,
                 userPropertiesJpaRepositoryProxy,
---
+++ src/main/java/com/github/olson1998/synthwave/service/authorizationserver/domain/service/oauth2/SimpleOAuth2AuthorizationService.java
@@ -0,0 +1,75 @@
+package com.github.olson1998.synthwave.service.authorizationserver.domain.service.oauth2;
+
+import com.github.olson1998.synthwave.service.authorizationserver.domain.port.oauth2.repository.OAuth2AuthorizationRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Slf4j
+@RequiredArgsConstructor
+public class SimpleOAuth2AuthorizationService implements OAuth2AuthorizationRepository {
+
+    private final Set<OAuth2Authorization> oAuth2Authorizations = new HashSet<>();
+
+    @Override
+    public void save(@NonNull OAuth2Authorization authorization) {
+        log.debug("Caching OAuth2 authorization: {}", authorization);
+        oAuth2Authorizations.add(authorization);
+    }
+
+    @Override
+    public void remove(@NonNull OAuth2Authorization authorization) {
+        log.debug("Removing OAuth2 authorization: {}", authorization);
+        oAuth2Authorizations.stream()
+                .filter(oAuth2Authorization -> oAuth2Authorization.getId().equals(authorization.getId()))
+                .findFirst()
+                .ifPresent(oAuth2Authorizations::remove);
+    }
+
+    @Override
+    public OAuth2Authorization findById(String id) {
+        log.debug("Searching for OAuth2 authorization: {}", id);
+        return oAuth2Authorizations.stream()
+                .filter(oAuth2Authorization -> oAuth2Authorization.getId().equals(id))
+                .findAny()
+                .orElse(null);
+    }
+
+    @Override
+    public OAuth2Authorization findByToken(String token, OAuth2TokenType tokenType) {
+        var tokenTypeValue = Optional.ofNullable(tokenType).map(OAuth2TokenType::getValue).orElse("{?}");
+        log.debug("Searching for OAuth2 authorization with type: {}", tokenTypeValue);
+        return oAuth2Authorizations.stream()
+                .filter(oAuth2Authorization -> isSameTokenAndType(oAuth2Authorization, token, tokenType))
+                .findAny()
+                .orElse(null);
+    }
+
+    private boolean isSameTokenAndType(OAuth2Authorization oAuth2Authorization, String token, OAuth2TokenType tokenType){
+        if(tokenType == null){
+            return oAuth2Authorization.getToken(token) != null;
+        } else if(tokenType.equals(OAuth2TokenType.ACCESS_TOKEN)){
+            return Optional.ofNullable(oAuth2Authorization.getAccessToken())
+                    .map(OAuth2Authorization.Token::getToken)
+                    .map(OAuth2AccessToken::getTokenValue)
+                    .stream()
+                    .anyMatch(tokenValue -> tokenValue.equals(token));
+        } else if (tokenType.equals(OAuth2TokenType.REFRESH_TOKEN)) {
+            return Optional.ofNullable(oAuth2Authorization.getRefreshToken())
+                    .map(OAuth2Authorization.Token::getToken)
+                    .map(OAuth2RefreshToken::getTokenValue)
+                    .stream()
+                    .anyMatch(tokenValue -> tokenValue.equals(token));
+        } else {
+            return oAuth2Authorization.getToken(token) != null;
+        }
+    }
+}
